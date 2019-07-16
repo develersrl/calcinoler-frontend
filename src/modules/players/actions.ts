725983971701a74a89a16865bd7d265af72e19e4
Ogek <ogek@develer.com>
2019-07-16 17:36:46 +0200

    Solved @gvaldambrini's review

--- src/modules/players/actions.ts
+++ src/modules/players/actions.ts
@@ -3,22 +3,29 @@ import {
   FETCH_PLAYERS_SUCCESS,
   FETCH_PLAYERS_ERROR
 } from "./constants";
-import { Player, PlayersAction } from "./types";
+import {
+  Player,
+  FetchPlayersAction,
+  FetchPlayersSuccessAction,
+  FetchPlayersErrorAction
+} from "./types";
 
-export function fetchPlayers(players: Player[]): PlayersAction {
+export function fetchPlayers(players: Player[]): FetchPlayersAction {
   return {
     type: FETCH_PLAYERS
   };
 }
 
-export function fetchPlayersSuccess(players: Player[]): PlayersAction {
+export function fetchPlayersSuccess(
+  players: Player[]
+): FetchPlayersSuccessAction {
   return {
     type: FETCH_PLAYERS_SUCCESS,
     payload: players
   };
 }
 
-export function fetchPlayersError(errors: string[]): PlayersAction {
+export function fetchPlayersError(errors: string[]): FetchPlayersErrorAction {
   return {
     type: FETCH_PLAYERS_ERROR,
     payload: errors
