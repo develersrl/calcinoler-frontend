7bba2c0778511ce61eb78409bd160e9463b7d089
Ogek <ogek@develer.com>
2019-07-18 16:58:09 +0200

    Added Redux Dev Tool

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import rootReducer from "./modules";
 
 import apiMiddleware from "./api/apiMiddleware";
 
-const store = createStore(rootReducer, applyMiddleware(apiMiddleware));
-
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(apiMiddleware))
+);
 
 export default store;
